0ab85a56e5881a166ba1ce457d7126715d3dfd71
raviteja <adusumalliraviteja@gmail.com>
2012-10-12 14:01:51 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinymce::Application.config.secret_token = 'f3d0b639319cd62e5fb34b96326e9f210912bfa4cbf16dd464e14afb0a9a083930a3a178a63b1aa3b0cb7bf21ee7f6c152dd68da14c654ae3b7eabff065da723'
